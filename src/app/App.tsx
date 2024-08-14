56715259f359fb40cb04964289b86e8eaa7b30ee
владислав <vladplatov3@yandex.ru>
2024-08-14 14:13:33 +0500

    Инициализация проекта

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import "./App.scss";
+import {classNames} from "shared/libs";
+
+function App() {
+
+    return (
+        <div className={classNames("")}>
+            test app
+        </div>
+    )
+}
+
+export default App;
